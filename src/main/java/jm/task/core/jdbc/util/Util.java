4a7bf5ff97812dd051a3bc6d51d56d9447cdac91
MuravevaAn <anna.tkacheva.91@bk.ru>
2022-05-19 18:29:54 +0300

    Completed task 1.1.3

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test_anna", "root", "root");
+
+        } catch (SQLException e) {
+            System.out.println("Не удалось подключиться к базе данных");
+        }
+        return connection;
+    }
 }
